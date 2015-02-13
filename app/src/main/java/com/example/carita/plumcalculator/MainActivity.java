1228227740817bb4c0145bd26b63f9efd7259f01
caritaou <caritaou@yahoo.com>
2015-02-13 00:02:16 -0800

    add button one listener

--- app/src/main/java/com/example/carita/plumcalculator/MainActivity.java
+++ app/src/main/java/com/example/carita/plumcalculator/MainActivity.java
@@ -4,16 +4,36 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-
+import android.view.View;
+import android.widget.Button;
+import android.view.View.OnClickListener;
+import android.widget.TextView;
 
 public class MainActivity extends ActionBarActivity {
+
+    TextView screen;
+    Button one;
+    Button two;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        screen = (TextView) findViewById(R.id.screen);
+        addButtons();
     }
 
+    public void addButtons() {
+        one = (Button) findViewById(R.id.one);
+        one.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                screen.setText("1");
+            }
+        });
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
