df149f2ab2ae502fc9cf30b32d35deecd48f6726
Carita Ou <carita.ou@gmail.com>
2015-02-13 13:13:19 -0800

    add button listeners

--- app/src/main/java/com/example/carita/plumcalculator/MainActivity.java
+++ app/src/main/java/com/example/carita/plumcalculator/MainActivity.java
@@ -11,10 +11,15 @@ import android.widget.TextView;
 
 public class MainActivity extends ActionBarActivity {
 
-    TextView screen;
-    Button one;
-    Button two;
+    private static final Integer MAX7DIGIT=88888888;
 
+    TextView screen;
+    Button one, two, three, four, five, six, seven, eight, nine, zero;
+    Button clear, plus, minus, evaluate;
+
+    String stack = "";
+    String operator = "";
+    int answer = MAX7DIGIT;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,7 +35,114 @@ public class MainActivity extends ActionBarActivity {
         one.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
-                screen.setText("1");
+                screen.append("1");
+            }
+        });
+
+        two = (Button) findViewById(R.id.two);
+        two.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                screen.append("2");
+            }
+        });
+
+        three = (Button) findViewById(R.id.three);
+        three.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                screen.append("3");
+            }
+        });
+
+        four = (Button) findViewById(R.id.four);
+        four.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                screen.append("4");
+            }
+        });
+
+        five = (Button) findViewById(R.id.five);
+        five.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                screen.append("5");
+            }
+        });
+
+        six = (Button) findViewById(R.id.six);
+        six.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                screen.append("6");
+            }
+        });
+
+        seven = (Button) findViewById(R.id.seven);
+        seven.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                screen.append("7");
+            }
+        });
+
+        eight = (Button) findViewById(R.id.eight);
+        eight.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                screen.append("8");
+            }
+        });
+
+        nine = (Button) findViewById(R.id.nine);
+        nine.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                screen.append("9");
+            }
+        });
+
+        zero = (Button) findViewById(R.id.zero);
+        zero.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                screen.append("0");
+            }
+        });
+
+        clear = (Button) findViewById(R.id.clear);
+        clear.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                screen.setText("");
+            }
+        });
+
+        plus = (Button) findViewById(R.id.plus);
+        plus.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                screen.append(" ");
+            }
+        });
+
+        minus = (Button) findViewById(R.id.minus);
+        minus.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                screen.append(" ");
+            }
+        });
+
+        evaluate = (Button) findViewById(R.id.equal);
+        evaluate.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(answer != MAX7DIGIT) {
+                    screen.append(" ");
+                    screen.append(String.valueOf(answer));
+                }
             }
         });
     }
